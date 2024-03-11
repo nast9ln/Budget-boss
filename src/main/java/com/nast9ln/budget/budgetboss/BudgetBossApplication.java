45df787929a8d1964a37f097f3876d8912149cca
nastya <nast9ln@vk.com>
2024-03-11 14:27:28 +0300

    Start project

---
+++ src/main/java/com/nast9ln/budget/budgetboss/BudgetBossApplication.java
@@ -0,0 +1,13 @@
+package com.nast9ln.budget.budgetboss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetBossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetBossApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nast9ln/budget/budgetboss/BudgetBossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nast9ln.budget.budgetboss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
